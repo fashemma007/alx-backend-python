da980702450bfad4f3645127c28a41a0c075c1b1
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-04-27 10:26:45 +0100

    feat[task-3]: to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""type-annotated function to_str"""
+
+
+def to_str(n: float) -> str:
+    """returns the string representation of the float passed as argument"""
+    return str(n)
