a80426b1a0a7de4dd6a2d757bbc34c55375da208
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-04-27 18:07:47 +0100

    feat[adv-1]: gets first element safely

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""The types of the elements of the input are not know"""
+
+from typing import Any, Sequence, Union
+
+
+# The types of the elements of the input are not know
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """The types of the elements of the input are not know"""
+    if lst:
+        return lst[0]
+    return None
