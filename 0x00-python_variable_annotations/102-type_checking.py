14d8ca32b6cd35487e105cab51e1658d5839de0e
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-04-27 20:46:00 +0100

    feat[adv-3]: fix annotation errors using mypy

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module Docs"""
+from typing import List, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """verifying witg mypy"""
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
