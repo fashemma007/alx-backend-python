defa8e0103119b2fde08761313b2cdb91254208d
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-04-27 12:30:40 +0100

    feat[task-7]: complex type annotations

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""type-annotated function to_kv"""
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Returns a tuple of string, float"""
+    return (k, v**2)
