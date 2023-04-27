11716108697853f6a7851f2f4afe649718b79f0d
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-04-27 09:44:26 +0100

    feat[task-0]: create a function with annotated params and return values

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""takes 2 floats a and b as arguments and returns their sum as a float"""
+
+
+def add(a: float, b: float) -> float:
+    """return the sum of 2 floating point numbers `a` and `b`"""
+    return a+b
