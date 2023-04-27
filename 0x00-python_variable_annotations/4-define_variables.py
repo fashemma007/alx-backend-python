2a7a3875ff3e748b7944bc4afacc21f1ef0eb0eb
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-04-27 10:37:57 +0100

    feat[task-4]: variable annotations

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""python variable annotations"""
+
+# a, an integer with a value of 1
+a: int = 1
+
+# pi, a float with a value of 3.14
+pi: float = 3.14
+
+# i_understand_annotations, a boolean with a value of True
+i_understand_annotations: bool = True
+
+# school, a string with a value of “Holberton”
+school: str = "Holberton"
