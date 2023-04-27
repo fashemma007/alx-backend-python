44c15347610a7522a1f1d1e256914929af6204e5
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-04-27 17:38:42 +0100

    feat[task-9]: use duck types for iterables and sequence

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module Docs"""
+
+
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """alx didnt gimme docstrings either"""
+    return [(i, len(i)) for i in lst]
---
+++ 0x00-python_variable_annotations/mains/9-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+element_length = __import__('9-element_length').element_length
+
+print(element_length.__annotations__)
+
+abc = element_length(['egg', 'banana', 'orange'])
+print(abc)
