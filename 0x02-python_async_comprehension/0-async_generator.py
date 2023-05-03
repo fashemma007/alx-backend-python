a8f228cdfbec53d581faf8fe66aa28d4cc10e40f
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-05-03 18:37:21 +0100

    feat[task-0]: asynchronous generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""module docs for 0-async_generator.py"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """will loop 10 times, each time asynchronously wait 1 second, then yield
+    a random number between 0 and 10
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        result = random.uniform(0, 10)
+        yield result
