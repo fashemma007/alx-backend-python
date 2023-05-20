3c6fef7649208c2b804b172bc6c89fe07f959bae
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-05-20 21:22:58 +0100

    test[get_json]: mock testing the get json method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Parameterize a unit test"""
 import unittest
+from unittest.mock import MagicMock, patch
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -24,6 +25,26 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test the access nested map function raised errors"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """test the get json function"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch("utils.requests")
+    def test_get_json(self, test_url, test_payload, mock_requests):
+        """test the get json function"""
+        # print(f"Here======>\n{test_payload}")
+        # bcos request.get returns a Response object,
+        # I need to mock a response object
+        mock_response = MagicMock()
+        # then imitate .json() method and  pass in my payload as return value
+        mock_response.json.return_value = test_payload
+        mock_requests.get.return_value = mock_response
+
+        self.assertEqual(get_json(test_url), test_payload)
 
 
 if __name__ == "__main__":
